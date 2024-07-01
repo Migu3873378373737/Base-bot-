94a2c643e037c796df91a2eab4dea44b90996cac
Migu3873378373737 <miguel.antonio.olicruvinel@gmail.com>
2024-07-01 12:29:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 
+@bot.event
+async def on_ready ():
+ print(f" \033[32m ☑️ conectado")
+ await bot.tree.sync()
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
