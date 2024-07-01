c23ab909ca66283f2b6ce675aa38736340de6cf5
Migu3873378373737 <miguel.antonio.olicruvinel@gmail.com>
2024-07-01 13:16:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 @bot.event
 async def on_ready ():
  print(f" \033[32m ☑️ conectado")
  await bot.tree.sync()
 
+    
+@bot.command(name='ping', help='Verificar o ping do bot')
+async def ping(ctx: commands.Context):
+    await ctx.send(f'🏓 Meu ping é {round(bot.latency * 1000)}ms')
 
 @bot.command()
-async def ping(ctx):
+async def teste(ctx):
     await ctx.send('pong')
 
 bot.run('token')
